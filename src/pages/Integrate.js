3105090b6a6ee8075d5b1e3aa87e31f96372c63b
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2020-09-26 15:31:07 +0530

    fix styles

--- src/pages/Integrate.js
+++ src/pages/Integrate.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Flex, Box, Button, Text } from "@chakra-ui/core";
-import { GoLogoGithub } from "react-icons/go";
-import { SiDiscord, SiHeroku, SiZoom, SiAsana } from "react-icons/si";
+import { Flex, Box, Button, Text, Heading } from "@chakra-ui/core";
+import { SiGithub, SiDiscord, SiHeroku, SiZoom, SiAsana } from "react-icons/si";
 
 const Integrate = () => {
   const [state, setState] = useState({
@@ -28,39 +27,44 @@ const Integrate = () => {
       direction="column"
     >
       <IntegrationCard
-        text={GoLogoGithub}
+        text={SiGithub}
         active={a}
+        label="Github"
         url={`https://github.com/login/oauth/authorize?scope=admin:org&client_id=${REACT_APP_GITHUB_CLIENT_ID}`}
       />
       <IntegrationCard
         text={SiZoom}
         active={b}
+        label="Zoom"
         url={`https://zoom.us/oauth/authorize?response_type=code&client_id=${REACT_APP_ZOOM_CLIENT_ID}&redirect_uri=http://localhost:3000/athr/zoom`}
       />
       <IntegrationCard
         text={SiDiscord}
         active={c}
+        label="Discord"
         url={`https://discord.com/api/oauth2/authorize?client_id=${REACT_APP_DISCORD_CLIENT_ID}&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fdiscord-auth&response_type=code&scope=guilds%20guilds.join%20identify%20email`}
       />
       <IntegrationCard
         text={SiAsana}
         active={d}
+        label="Asana"
         url={`https://app.asana.com/-/oauth_authorize?client_id=${REACT_APP_ASANA_CLIENT_ID}&redirect_uri=http://localhost:3000/asana-auth&response_type=code`}
       />
       <IntegrationCard
         text={SiHeroku}
         active={e}
+        label="Heroku"
         url={`https://id.heroku.com/oauth/authorize?client_id=${REACT_APP_HEROKU_CLIENT_ID}&response_type=code&scope=global&state=${REACT_APP_ANTI_FORGERY}`}
       />
     </Flex>
   );
 };
 
-const IntegrationCard = ({ text, active, color = null, url }) => (
+const IntegrationCard = ({ text, active, color = null, url, label = null }) => (
   <Box
     p={4}
     m={3}
-    maxWidth="500px"
+    maxWidth="350px"
     borderWidth={1}
     borderRadius={8}
     boxShadow="lg"
@@ -68,6 +72,7 @@ const IntegrationCard = ({ text, active, color = null, url }) => (
   >
     <Flex align="center" justifyContent="space-between">
       <Box as={text} size="64px" color={color} />
+      {/* <Text fontSize="xl">{label}</Text> */}
       <a href={url}>
         <Button
           mt={4}
