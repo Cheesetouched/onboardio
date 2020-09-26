ac938e127f2e2f980eb0f5d0e3c67cb97d3c0c27
Utkarsh Dixit <utkarshdix02@gmail.com>
2020-09-26 13:05:49 +0530

    Add command to start server and app together

--- server/api/middlewares/requestLogger.ts
+++ server/api/middlewares/requestLogger.ts
@@ -8,7 +8,7 @@ export const RequestLogger = (req, res, next) => {
     const elapsedHrTime = process.hrtime(startHrTime);
     const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6;
     Logger.info(
-      chalk.cyanBright.bold(req.baseUrl + req.path),
+      chalk.cyanBright.bold(req.path),
       `${chalk.whiteBright.bold(res.statusCode)} (${chalk.whiteBright.bold(
         elapsedTimeInMs
       )} ms)`
