7f2c9d5ca1e1febd0734abadd5df927365be206b
Utkarsh Dixit <utkarshdix02@gmail.com>
2020-09-26 22:44:47 +0530

    Add CLIENT_URL in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const BACKEND_SERVER_URL = process.env.NODE_ENV === "production" ? "https://backend-onboardio.heroku-app.com" : "http://localhost:8080/";
+export const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://onboardio.heroku-app.com/": "http://localhost:3000/";
