bc9c1f190c419bd55abb0b00a19cdc1335ad23b6
Cheesetouched <pingrishabh@gmail.com>
2020-09-27 19:07:27 +0530

    Fixed and tested all services
    - GitHub fixed
    - Heroku fixed
    - Asana fixed
    - Auto-token update done

--- server/api/services/service.ts
+++ server/api/services/service.ts
@@ -7,27 +7,49 @@ const User = mongoose.model("User", UserModel);
 
 export class ServiceProviderService {
   static saveService(payload) {
-    return new Promise((resolve, reject) => {
-      User.updateOne(
-        { email: payload.email },
-        { $push: { services: { name: payload.name, token: payload.token } } },
-        (err, updated) => {
+    return new Promise(async (resolve, reject) => {
+      User.findOne(
+        { email: payload.email, "services.name": payload.name },
+        (err, service) => {
           if (err) reject({ code: 500, message: err });
-          if (updated.nModified == 1) resolve("Service linked successfully!");
-          else reject({ code: 500, message: "Failed to save the service." });
+          let query;
+          let find;
+          if (service) {
+            find = { email: payload.email, "services.name": payload.name };
+            query = {
+              $set: {
+                "services.$": { name: payload.name, token: payload.token },
+              },
+            };
+          } else {
+            find = { email: payload.email };
+            query = {
+              $push: { services: { name: payload.name, token: payload.token } },
+            };
+          }
+          User.updateOne(find, query, (err, updated) => {
+            if (err) reject({ code: 500, message: err });
+            if (updated.nModified == 1) resolve("Service linked successfully!");
+            else reject({ code: 500, message: "Failed to save the service." });
+          });
         }
       );
+
+      /*
+
+      
+      */
     });
   }
 
   static linkGithub(code: string) {
     return new Promise((resolve, reject) => {
       const params = new URLSearchParams();
-      params.append("grant_type", "authorization_code");
-      params.append("code", code);
+      params.append("client_id", process.env.GITHUB_CLIENT_ID);
       params.append("client_secret", process.env.GITHUB_CLIENT_SECRET);
+      params.append("code", code);
 
-      fetch(process.env.GITHUB_OAUTH_ACCESS_TOKEN, {
+      fetch(process.env.GITHUB_TOKEN_EXCHANGE_URL, {
         method: "post",
         body: params,
         headers: { Accept: "application/json" },
@@ -48,15 +70,14 @@ export class ServiceProviderService {
       params.append("code", code);
       params.append("client_secret", process.env.HEROKU_CLIENT_SECRET);
 
-      fetch(process.env.HEROKU_CLIENT_ACCESS_TOKEN_URL, {
+      fetch(process.env.HEROKU_TOKEN_EXCHANGE_URL, {
         method: "post",
         body: params,
-        headers: { Accept: "application/json" },
       })
         .then((res) => res.json())
         .then((response) => {
-          if (response.error)
-            reject({ code: 500, message: response.error_description });
+          if (response.id == "unauthorized")
+            reject({ code: 500, message: response.message });
           else resolve(response.access_token);
         });
     });
@@ -68,13 +89,12 @@ export class ServiceProviderService {
       params.append("grant_type", "authorization_code");
       params.append("client_id", process.env.ASANA_CLIENT_ID);
       params.append("client_secret", process.env.ASANA_CLIENT_SECRET);
-      params.append("code", code);
+      params.append("code", unescape(code));
       params.append("redirect_uri", redirect_uri);
 
-      fetch(process.env.ASANA_CLIENT_ACCESS_TOKEN_URL, {
+      fetch(process.env.ASANA_TOKEN_EXCHANGE_URL, {
         method: "post",
         body: params,
-        headers: { Accept: "application/json" },
       })
         .then((res) => res.json())
         .then((response) => {
