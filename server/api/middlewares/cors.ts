f79bb2bf31b0e9f4fe68c0090152bd197288bc99
Utkarsh Dixit <utkarshdix02@gmail.com>
2020-09-27 20:16:54 +0530

    Allow authorization in Access-Control-Allow-Headers

--- server/api/middlewares/cors.ts
+++ server/api/middlewares/cors.ts
@@ -4,7 +4,7 @@ export const CorsMiddleware = (req, res, next) => {
     }
     res.header('Access-Control-Allow-Credentials', `true`);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
     if (req.method === 'OPTIONS') {
         res.sendStatus(200);
