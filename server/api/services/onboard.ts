34f737e17a79f6316e3a4c15b25a472afbcc24c0
Utkarsh Dixit <utkarshdix02@gmail.com>
2020-09-29 17:19:09 +0530

    Send zoho invites

--- server/api/services/onboard.ts
+++ server/api/services/onboard.ts
@@ -1,66 +1,92 @@
 import * as mongoose from "mongoose";
-import { UserModel } from "../models/user";
+import {UserModel} from "../models/user";
+import * as fetch from "node-fetch";
 
 const User = mongoose.model("User", UserModel);
 
 function sendAsanaInvite() {
-  return new Promise((resolve, reject) => {
-    resolve({ asana: true });
-  });
+    return new Promise((resolve, reject) => {
+        resolve({asana: true});
+    });
 }
 
 function sendDiscordInvite() {
-  return new Promise((resolve, reject) => {
-    reject({ discord: "403: Forbidden" });
-  });
+    return new Promise((resolve, reject) => {
+        reject({discord: "403: Forbidden"});
+    });
 }
 
 function sendGitHubInvite() {
-  return new Promise((resolve, reject) => {
-    resolve({ github: true });
-  });
+    return new Promise((resolve, reject) => {
+        resolve({github: true});
+    });
 }
 
 function sendHerokuInvite() {
-  return new Promise((resolve, reject) => {
-    resolve({ heroku: true });
-  });
+    return new Promise((resolve, reject) => {
+        resolve({heroku: true});
+    });
 }
 
-function sendZohoInvite() {
-  return new Promise((resolve, reject) => {
-    reject({ zoho: "Access token expired" });
-  });
+function sendZohoInvite(emails, token, meta) {
+    return new Promise((resolve, reject) => {
+        const usersArr = emails.map(email => {
+            return {
+                "role": meta["zoho"].role,
+                "first_name": "John",
+                "email": email,
+                "profile": meta["zoho"].profile,
+                "last_name": "Doe"
+            };
+        });
+
+        return fetch(`https://www.zohoapis.com/crm/v2/users`, {
+            method: "post",
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            data: {
+                users: usersArr
+            }
+        })
+            .then((res) => res.json())
+            .then((response) => {
+                if (response.error) {
+                    reject({code: 500, message: response.error});
+                }
+                resolve(response);
+            });
+    });
 }
 
 export class OnboardService {
-  static onboardUsers(services: any, flow: any, emails: Array<string>) {
-    return new Promise(async (resolve) => {
-      let report = [];
-      let promiseArray = [];
+    static onboardUsers(services: any, flow: any, emails: Array<string>) {
+        return new Promise(async (resolve) => {
+            let report = [];
+            let promiseArray = [];
 
-      flow.services.forEach((flowService) => {
-        services.forEach((service) => {
-          if (flowService == service.id) {
-            if (service.name == "Asana") promiseArray.push(sendAsanaInvite());
-            if (service.name == "Discord")
-              promiseArray.push(sendDiscordInvite());
-            if (service.name == "GitHub") promiseArray.push(sendGitHubInvite());
-            if (service.name == "Heroku") promiseArray.push(sendHerokuInvite());
-            if (service.name == "Zoho") promiseArray.push(sendZohoInvite());
-          }
-        });
-      });
+            flow.services.forEach((flowService) => {
+                services.forEach((service) => {
+                    if (flowService == service.id) {
+                        if (service.name == "Asana") promiseArray.push(sendAsanaInvite());
+                        if (service.name == "Discord")
+                            promiseArray.push(sendDiscordInvite());
+                        if (service.name == "GitHub") promiseArray.push(sendGitHubInvite());
+                        if (service.name == "Heroku") promiseArray.push(sendHerokuInvite());
+                        if (service.name == "Zoho") promiseArray.push(sendZohoInvite(emails, service.token, flow.meta));
+                    }
+                });
+            });
 
-      emails.forEach((email, index) => {
-        Promise.allSettled(promiseArray).then((result) => {
-          report.push({
-            email: email,
-            result: result,
-          });
-          if (index == emails.length - 1) resolve(report);
+            emails.forEach((email, index) => {
+                Promise.allSettled(promiseArray).then((result) => {
+                    report.push({
+                        email: email,
+                        result: result,
+                    });
+                    if (index == emails.length - 1) resolve(report);
+                });
+            });
         });
-      });
-    });
-  }
+    }
 }
